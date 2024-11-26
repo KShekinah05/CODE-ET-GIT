192da5a123188c59419fdecab3a5e9ed30822d36
KOTTE TCHOUBANG Shekinah <kottetcs@3il.fr>
2024-11-26 14:55:18 +0100

    Simplification dde l'exercice2 et ajout de quelque commmentaires faute de temps

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,45 @@
+
+
 public class GestionnaireNotes {
 
     // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+    /
+     * affiche le nom de l'étudiant et toutes ses notes
+     * 
+     * @param nomEtudiant le nom de l'eutiant
+     * @param notes tableau contenant les notes de l'étudiant
+     */
+    public void afficherNotes(String nomEtudiant, int[] notes) 
+    {
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
+    }
 
-        // Calcul de la somme des notes
-        int somme = 0;
-        for (int note : notes) {
+    /
+     * 
+     * calcule la somme des notes d'un étudiant
+     * @param notes tableau contenant les notes de l'étudiant
+     * @return la somme des notes d'un élève
+     */
+    public int  calculersomme(int[] notes) {
+        int somme = 0;// variable qui va contenir la somme des notes
+        for (int note : notes) {//calculer la somme des notes de l'étudiant
             somme += note;
         }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        return somme;
     }
-}
+
+    /**
+     * affiche la moyenne d'un etidiant
+     * 
+     * @param notes tableau contenant les notes de l'étudiant
+     * @param nomEtudiant le nom de l'eutiant
+     */
+       public void afficherMoyenne(String nomEtudiant,int[] notes){
+        double moyenne = calculersomme(notes)/ notes.length;
+        System.out.println("Moyenne de " + nomEtudiant + ":" + moyenne);
+       }
+        
+    }
